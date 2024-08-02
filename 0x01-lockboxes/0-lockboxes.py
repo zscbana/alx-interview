ec9535f2c69848eabfcbd9c35cb95ac7567643c0
zscbana <zscbana@gmail.com>
2024-08-02 05:47:01 +0300

    test 1

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Solves the lock boxes puzzle """
+
+
+def canUnlockAll(boxes):
+    """Solves the lock boxes puzzle """
+    n = len(boxes)
+    unlocked = set()
+    queue = [0]  
+    unlocked.add(0)
+    
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if key < n and key not in unlocked:
+                unlocked.add(key)
+                queue.append(key)
+    
+    return len(unlocked) == n
