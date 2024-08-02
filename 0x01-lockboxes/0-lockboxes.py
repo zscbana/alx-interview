c34e4597b681e86cea933359d0ffd1c6bb9b2ae5
zscbana <zscbana@gmail.com>
2024-08-02 05:48:07 +0300

    code style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,14 +6,14 @@ def canUnlockAll(boxes):
     """Solves the lock boxes puzzle """
     n = len(boxes)
     unlocked = set()
-    queue = [0]  
+    queue = [0]
     unlocked.add(0)
-    
+
     while queue:
         current_box = queue.pop(0)
         for key in boxes[current_box]:
             if key < n and key not in unlocked:
                 unlocked.add(key)
                 queue.append(key)
-    
+
     return len(unlocked) == n
