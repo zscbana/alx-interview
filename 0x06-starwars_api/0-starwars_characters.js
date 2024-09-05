fb19ec03d988281a5c7ad9d0567b4d5431cfdaed
zscbana <zscbana@gmail.com>
2024-09-05 19:06:43 +0300

    code style

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require('request')
 
-const filmId = process.argv[2];
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+const filmId = process.argv[2]
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${filmId}`
 
 request(apiUrl, (error, response, body) => {
-  if (error) throw error;
-  
-  const characterUrls = JSON.parse(body).characters;
-  fetchCharactersInOrder(characterUrls, 0);
-});
+  if (error) throw error
+
+  const characterUrls = JSON.parse(body).characters
+  fetchCharactersInOrder(characterUrls, 0)
+})
 
 const fetchCharactersInOrder = (characterUrls, index) => {
-  if (index === characterUrls.length) return;
+  if (index === characterUrls.length) return
 
   request(characterUrls[index], (error, response, body) => {
-    if (error) throw error;
+    if (error) throw error
 
-    console.log(JSON.parse(body).name);
-    fetchCharactersInOrder(characterUrls, index + 1);
-  });
-};
+    console.log(JSON.parse(body).name)
+    fetchCharactersInOrder(characterUrls, index + 1)
+  })
+}
