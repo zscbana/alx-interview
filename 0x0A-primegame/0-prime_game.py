8e659aad8363b3af71d544d5217d522924b282a4
zscbana <zscbana@gmail.com>
2024-10-03 21:39:25 +0300

    prime Game V1.0

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""0. Prime Game"""
+
+
+def isWinner(x, nums):
+    """Check if player x wins"""
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_mul(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_mul(ls, x):
+    """Remove multiples of prime number x from the list `ls`"""
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
